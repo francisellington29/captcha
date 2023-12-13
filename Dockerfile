742da034653c45dbfb6c2522cd9a803d0250dc6e
francisellington29 <141621117+francisellington29@users.noreply.github.com>
2023-12-13 10:52:46 +0000

    update

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.9-alpine
+WORKDIR /code
+COPY . /code
+
+RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -19,8 +19,7 @@ app.add_middleware(
 timezone = pytz.timezone('Asia/Shanghai')
 
 def current_time():
-    current_time = datetime.now()
-    current_time.astimezone(timezone)
+    current_time = datetime.now(timezone)
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_time
 
