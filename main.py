3e666e194d084842b0411bbf932439fcc0c6b853
francisellington29 <141621117+francisellington29@users.noreply.github.com>
2023-12-13 17:36:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from gxp import api
 
 app = FastAPI()
 
+# 允许所有来源访问，更具体的配置可以根据需求进行修改
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源访问，也可以指定特定的来源，如 ["http://localhost", "https://example.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有 HTTP 方法访问
+    allow_headers=["*"],  # 允许所有头部访问
+)
 
 @app.get("/")
 def get_balance(api_key: str):
